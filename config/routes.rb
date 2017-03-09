1151ad6f00396488232dcbda8acf056ed27d3797
alimun88 <ayon88@hotmail.com>
2017-03-09 18:18:59 +0000

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   devise_for :users
   resources :contacts, only: :create
-  get 'contact_us', to: 'contacts#new', as: 'new_contact'
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
   
 end
