ace9c6d3ce7c756a2d4722f9824bf07f5c21385a
alimun88 <ayon88@hotmail.com>
2017-03-09 05:19:33 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :contacts, only: [:create]
+  resources :contacts, only: :create
   get 'contact_us', to: 'contacts#new', as: 'new_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
