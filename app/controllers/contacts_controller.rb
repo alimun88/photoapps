fe1c7e5016e92e029b6e83182780d23e2818b392
alimun88 <ayon88@hotmail.com>
2017-03-09 02:15:04 +0000

    Correction made to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,6 @@
 class ContactsController < ApplicationController
     skip_before_action :authenticate_user!
     
-  name = params[:contact][:name]
-  email = params[:contact][:email]
-  body = params[:contact][:comments]
-  ContactMailer.contact_email(name, email, body).deliver
-
   def new
     @contact = Contact.new
   end
@@ -13,6 +8,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent Susscessfully"
       redirect_to new_contact_path
     else
@@ -24,6 +23,6 @@ class ContactsController < ApplicationController
   private
   
   def contact_params
-   params.require(:contact).permit(:name, :email, :comments)
+    params.require(:contact).permit(:name, :email, :comments)
   end
 end
